d713f45c0b57459157c792fb7e33ce4fabb9e0f1
atish <atish.lewate@kunalinfotech.net>
2013-11-07 13:00:09 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Drive::Application.config.secret_token = 'c5a9cf6682af6141c4c42776502c5d3dd8188828d4434286e53ed4970714b34847a59d0f0b6165f3bc01f67341a240bc1bbcd22177bb24c62d46db4f47247747'
