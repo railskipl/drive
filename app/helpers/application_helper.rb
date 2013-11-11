1211da12bf08bd93a4fdf7bef697dfadadf961ba
kipl <kipl@kipl-Inspiron-620s.(none)>
2013-11-11 11:36:40 +0530

    user management

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+
+
+ def format_boolean(approved)
+        if approved
+          icon_tick
+        else
+          icon_cross
+        end
+      end
+      
+      def icon_tick(alt_text='Tick')
+        build_image_tag("/assets/001_06.png", alt_text)
+      end
+
+      def icon_cross(alt_text='Cross')
+        build_image_tag("/assets/001_05.png", alt_text)
+      end
+      
+      def build_image_tag(image_file, alt_text)
+        image_tag(image_file, :size =>"20x20", :alt => alt_text)
+      end
+      
+        def link_to_remove_fields(name, f)
+    f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)")
+  end
+
 end
