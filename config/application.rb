015d588850aa58523684bbdb1840a64987e58496
kipl <kipl@kipl-Inspiron-620s.(none)>
2013-11-13 15:30:18 +0530

    change

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,9 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+
+
+
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
@@ -11,6 +14,8 @@ end
 
 module Drive
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
