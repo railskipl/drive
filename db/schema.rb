053d113b92dba4da9268696966ac011d2297c711
kipl <kipl@kipl-Inspiron-620s.(none)>
2013-11-13 11:21:15 +0530

    removed raise

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
-ActiveRecord::Schema.define(:version => 20131112102735) do
-
+ActiveRecord::Schema.define(:version => 20131112190717) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
