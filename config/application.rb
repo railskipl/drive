f92873527c5b26d005ff91a8d85aa7678b27d4bc
kipl <kipl@kipl-Inspiron-620s.(none)>
2013-11-13 12:19:50 +0530

    changes

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ if defined?(Bundler)
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
+config.assets.initialize_on_precompile = false
+
 
 module Drive
   class Application < Rails::Application
