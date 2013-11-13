a1afad6f11a6259e43d4ed51f8e7e6cb0f29c0d3
kipl <kipl@kipl-Inspiron-620s.(none)>
2013-11-13 12:31:52 +0530

    changes

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,6 @@ end
 
 
 module Drive
-    config.assets.initialize_on_precompile = false
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
