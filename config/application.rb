4052ade37d173fb0c2b1fd0f8b2c59fcac880536
kipl <kipl@kipl-Inspiron-620s.(none)>
2013-11-15 11:38:45 +0530

    application.rb

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module Drive
 
   class Application < Rails::Application
 
+    #Added comment
     config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
