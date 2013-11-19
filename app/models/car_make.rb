36fc7c08ad87bfea4c3e54f73aaa2b623d3d00f1
kipl <kipl@kipl-Inspiron-620s.(none)>
2013-11-19 17:56:57 +0530

    added dependent destroy

--- app/models/car_make.rb
+++ app/models/car_make.rb
@@ -1,8 +1,8 @@
 class CarMake < ActiveRecord::Base
   attr_accessible :name
   has_many :car_models,  dependent: :destroy
-  has_many :carprofiles
-  has_many :logbooks
+  has_many :carprofiles, dependent: :destroy
+  has_many :logbooks, dependent: :destroy
 
   validates_presence_of :name
 
