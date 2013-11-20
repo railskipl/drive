c01e7f965d376648393befa7c61295da976a6728
root <root@piyush-Inspiron-620s.(none)>
2013-11-20 11:09:32 +0530

    model validations

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -6,5 +6,5 @@ class Admin < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
-   validates_uniqueness_of :email, :password
+ 
 end
