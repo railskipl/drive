78dac33caeb0063d9c25c02571bb2a39d2831504
kipl <kipl@kipl-Inspiron-620s.(none)>
2013-12-11 13:00:56 +0530

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
-ActiveRecord::Schema.define(:version => 20131210104547) do
-
+ActiveRecord::Schema.define(:version => 20131210132810) do
 
   create_table "admincontacts", :force => true do |t|
     t.string   "emailid"
@@ -105,8 +103,8 @@ ActiveRecord::Schema.define(:version => 20131210104547) do
     t.string   "manufacturing_year"
     t.string   "year_of_purchase"
     t.string   "whatkindofcar"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.text     "car_description"
     t.string   "power"
     t.string   "sellthiscar"
@@ -177,8 +175,8 @@ ActiveRecord::Schema.define(:version => 20131210104547) do
 
   create_table "egifts", :force => true do |t|
     t.integer  "credit"
-    t.datetime "created_at",         :null => false
-    t.datetime "updated_at",         :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
