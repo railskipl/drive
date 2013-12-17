21eb605032926f6b4915a3ca8ad6c9c920510761
kipl <kipl@kipl-Inspiron-620s.(none)>
2013-12-17 11:51:29 +0530

    bug fixes dec 16

--- app/models/carprofile.rb
+++ app/models/carprofile.rb
@@ -7,11 +7,14 @@ class Carprofile < ActiveRecord::Base
   belongs_to :body_index
   belongs_to :car_make
   belongs_to :car_model
-  has_many :send_gifts
+  has_many :send_gifts, dependent: :destroy
   has_many :favourites,dependent: :destroy
   has_many :carprofile_photos, dependent: :destroy
   has_many :logbooks, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  validates_length_of :car_description,:maximum => 2000
+
 
   validates_presence_of :year_of_purchase,:license_plate,:sellthiscar,:car_description,:power,:whatkindofcar,:manufacturing_year,:car_make_id,:car_model_id,:body_index_id
 
