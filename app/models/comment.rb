b8076b376f48cbeb0b7d77f501d7201a731c85c8
kipl <kipl@kipl-Inspiron-620s.(none)>
2013-12-23 11:04:06 +0530

    error handling

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ActiveRecord::Base
   attr_accessible :body, :carprofile_id,:user_id
   belongs_to :carprofile,:counter_cache => true
   belongs_to :user
- validates_length_of :body,:maximum => 2000
+
   def self.add_comment(body,user,carprofile)
   	self.create(:carprofile_id => carprofile.id,:body => body, :user_id => user.id)
   end
