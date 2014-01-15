954d41e5e2c90499d859301d2b5afdd9f80f6682
atish <atish.lewate@kunalinfotech.net>
2014-01-15 16:09:27 +0530

    validations on abuse reports

--- app/models/abuse_report.rb
+++ app/models/abuse_report.rb
@@ -1,6 +1,7 @@
 class AbuseReport < ActiveRecord::Base
   attr_accessible :abuse_category_id, :abuse_type, :abuser_user_id, :carprofile_id, :comment_id, :comments, :logbook_id, :user_blog_id, :user_id
-
+  
+  validates_presence_of :abuse_category_id
   belongs_to :abuse_category
   belongs_to :user
 end
