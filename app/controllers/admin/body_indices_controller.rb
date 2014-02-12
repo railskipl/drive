fa4ad2db3c886a0c21502a4678ea0b7b8050245c
atish <atish.lewate@kunalinfotech.net>
2014-02-12 17:23:26 +0530

    updated car make, model and body index

--- app/controllers/admin/body_indices_controller.rb
+++ app/controllers/admin/body_indices_controller.rb
@@ -7,10 +7,16 @@ class Admin::BodyIndicesController < ApplicationController
 
   def new
   	@body_index = BodyIndex.new
+    @car_models = CarModel.all
   end
 
   def edit
-   @body_index = BodyIndex.find(params[:id])  
+   @body_index = BodyIndex.find(params[:id])
+   @car_models = []
+   session[:car_model_id] = @body_index.car_model_id
+   if session[:car_model_id]
+     @car_models = CarModel.find_all_by_id(session[:car_model_id])
+   end
   end
 
   def create
@@ -27,6 +33,8 @@ class Admin::BodyIndicesController < ApplicationController
   def update
   	@body_index = BodyIndex.find(params[:id])
   	if @body_index.update_attributes(params[:body_index])
+      session.delete(:car_model_id)
+      
   		flash[:notice] = "Updated Successfully"
       if params[:type] == "Update Body Index"
        redirect_to manage_bodyindices_path
